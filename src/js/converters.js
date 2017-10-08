0e89344958b66a756786d13a1c941d3cd4baa56a
qooban <jakub.niezgoda@nitarch.pl>
2017-10-08 15:07:52 +0200

    Implemented Distance/Elevation API (edk-route-verifier-3) Optimized usage of turf

--- src/js/converters.js
+++ src/js/converters.js
@@ -1,30 +1,60 @@
 var toGeoJSON = require('togeojson');
 var _ = require('lodash');
 
-module.exports.getGeoJSON = function(xml) {
+function getGeoJSON(xml) {
     return toGeoJSON.kml(xml);
 }
 
-module.exports.getPathElevations = function(geoJson) {
+function getLineString(geoJson) {
     var lineString = _.find(geoJson.features, function (feature) {
         return _.isEqual(feature.geometry.type, 'LineString')
     });
-    var path = _.map(lineString.geometry.coordinates, function (element) {
-        return {lat: element[0], lng: element[1]};
-    });
+    return lineString;
+}
 
-    return new Promise(function(resolve,reject) {
-        var elevator = new google.maps.ElevationService;
-        var maximumNumberOfSamples = 512;
-        elevator.getElevationAlongPath({
-            'path': path,
-            'samples': _.min([path.length, maximumNumberOfSamples])
-        }, function(elevations, status) {
-            if (status == google.maps.ElevationStatus.OK) {
-                resolve(elevations);
-            } else {
-                reject(status);
-            }
-        });
+function getGoogleMapsPath(lineString) {
+    var path = _.map(lineString.geometry.coordinates, function (element) {
+        return new google.maps.LatLng(element[0], element[1]);
     });
+    return path;
+}
+
+function getPathElevations(geoJson) {
+    var lineString = getLineString(geoJson);
+
+    if (lineString.geometry.coordinates[0].length === 3) {
+        // Elevation present in line string
+        var elevations = _.map(lineString.geometry.coordinates, function (element) {
+            return {elevation: element[2]};
+        });
+
+        return new Promise(function(resolve,reject) {
+            resolve(elevations);
+        })
+    } else {
+        // No elevation in line string
+        var path = getGoogleMapsPath(lineString);
+
+        return new Promise(function(resolve,reject) {
+            var elevator = new google.maps.ElevationService;
+            var maximumNumberOfSamples = 512;
+            elevator.getElevationAlongPath({
+                'path': path,
+                'samples': _.min([path.length, maximumNumberOfSamples])
+            }, function(elevations, status) {
+                if (status == google.maps.ElevationStatus.OK) {
+                    resolve(elevations);
+                } else {
+                    reject(status);
+                }
+            });
+        });
+    }
+}
+
+module.exports = {
+    getGeoJSON: getGeoJSON,
+    getLineString: getLineString,
+    getGoogleMapsPath: getGoogleMapsPath,
+    getPathElevations: getPathElevations
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 var converters = require('./js/converters');
+var geometry = require('./js/geometry');
 var helpers = require('./js/helpers');
 
 // TODO: Remove it after getting route ID from context implemented
 var testRouteId = '9d13a0acad3ef4da05da04efba7a779e8d91f592';
+testRouteId = '167fb6420c15fa6f155c091bf9dde192bd7e5309';
+testRouteId = '56adb3f60f82870e833daaaab2301f49ab6e032b';
 
 function verifyRoute() {
     // TODO: Get route ID from context
@@ -10,15 +13,25 @@ function verifyRoute() {
         .then(function (data) {
             // TODO: Add XML validation
             var geoJson = converters.getGeoJSON(data);
-            console.log(geoJson);
+            console.log('GeoJSON: ', geoJson);
+
+            var pathLengthTurf = geometry.getPathLength(geoJson, true);
+            console.log('Path length:', pathLengthTurf.toFixed(2), 'kilometers');
+            var pathLengthGM = geometry.getPathLength(geoJson, false);
+            console.log('Path length:', pathLengthGM.toFixed(2), 'meters');
 
             // TODO: Implement route verification
             converters.getPathElevations(geoJson)
             .then(function(elevations) {
                 console.log(elevations);
+                var elevationGain = geometry.getElevationGain(elevations);
+                console.log('Elevation gain:', elevationGain.toFixed(2), 'meters');
+                var elevationLoss = geometry.getElevationLoss(elevations);
+                console.log('Elevation loss:', elevationLoss.toFixed(2), 'meters');
+                console.log('Elevation total change:', geometry.getTotalElevationChange(elevationGain, elevationLoss).toFixed(2), 'meters');
             })
             .catch(function(error) {
-                console.log('dupa')
+
             });
         }).catch(
         function (data, error) {
