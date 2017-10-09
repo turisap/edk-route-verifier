31320fc0007e05e73f5a19fef542fa3d356207b2
qooban <jakub.niezgoda@nitarch.pl>
2017-10-09 23:46:54 +0200

    Implemented Stations API (edk-route-verifier-4) Refactored routeVerifer to use Route and PathElevation objects. Merged converters and helpers.

---
+++ src/js/PathElevation.js
@@ -0,0 +1,23 @@
+module.exports = function(elevations) {
+    var getGain = function(elevations) {
+        var elevationGain = 0.0;
+        for(var i = 1; i < elevations.length; i++) {
+            var elevationDifference = elevations[i].elevation - elevations[i-1].elevation;
+            elevationGain += (elevationDifference > 0) ? elevationDifference : 0.0;
+        }
+        return Number(elevationGain);
+    }
+    var getLoss = function(elevations) {
+        var elevationLoss = 0.0;
+        for(var i = 1; i < elevations.length; i++) {
+            var elevationDifference = elevations[i-1].elevation - elevations[i].elevation;
+            elevationLoss += (elevationDifference > 0) ? elevationDifference : 0.0;
+        }
+        return Number(elevationLoss);
+    }
+
+    this.gain = getGain(elevations);
+    this.loss = getLoss(elevations);
+    this.totalChange = this.loss + this.gain;
+}
+
--- src/loadMap.js
+++ src/loadMap.js
@@ -1,5 +1,14 @@
 var map;
-var src = 'http://rejony.edk.org.pl/api/edk/route-files/56adb3f60f82870e833daaaab2301f49ab6e032b/download/gps';
+
+//TODO: Remove
+var testRouteId = [
+    '9d13a0acad3ef4da05da04efba7a779e8d91f592',
+    '167fb6420c15fa6f155c091bf9dde192bd7e5309',
+    '56adb3f60f82870e833daaaab2301f49ab6e032b',
+    'd53606410e75db67d35bff07b49846f5d4ed0bc5']
+
+var routeId = '167fb6420c15fa6f155c091bf9dde192bd7e5309';
+var src = 'http://rejony.edk.org.pl/api/edk/route-files/' + routeId +'/download/gps';
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
